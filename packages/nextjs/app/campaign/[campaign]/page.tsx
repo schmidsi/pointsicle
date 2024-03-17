7b4b4bd6e6634cbfe62bcc7c70bbc781bade8c06
a-bolsunovsky <a.bolsunovsky@icloud.com>
2024-03-17 08:51:20 +0000

    fixed camp

--- packages/nextjs/app/campaign/[campaign]/page.tsx
+++ packages/nextjs/app/campaign/[campaign]/page.tsx
@@ -1,7 +1,9 @@
 import CampaignPage from './data'
+import { columns } from "./_components/columns"
+import { DataTable } from "./_components/data-table"
 
-  const campaignCall = async (subgraphId: string) => {
-    const QUERY = `
+const campaignCall = async (subgraphId: string) => {
+  const QUERY = `
     query {
       pointsicleCampaigns {
         id
@@ -30,28 +32,28 @@ import CampaignPage from './data'
         }
     }`;
 
-    
-    const response = await fetch(`https://gateway-arbitrum.network.thegraph.com/api/${'e8c6e4f2a7fc3b832f3917a050e8057a'}/subgraphs/id/${subgraphId}`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        query: QUERY,
- 
-      }),
-    });
 
-    if (!response.ok) {
-      throw new Error('Failed to fetch data from GraphQL endpoint');
-    }
+  const response = await fetch(`https://gateway-arbitrum.network.thegraph.com/api/${'e8c6e4f2a7fc3b832f3917a050e8057a'}/subgraphs/id/${subgraphId}`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      query: QUERY,
 
-    const data = await response.json();
-    return data;
-  };
+    }),
+  });
+
+  if (!response.ok) {
+    throw new Error('Failed to fetch data from GraphQL endpoint');
+  }
+
+  const data = await response.json();
+  return data;
+};
 
 
-const DashboardPage = async ({params}: {
+const DashboardPage = async ({ params }: {
   params: {
     campaign: string
   }
@@ -59,11 +61,73 @@ const DashboardPage = async ({params}: {
 
 
   let data = await campaignCall(params.campaign)
+
+  if (!data) {
+    return <>GraphQL request returned an error for this one, oops</>
+  }
+
+
+  const tasks = data.pointsicleUsers ? data.pointsicleUsers.map((user: any) => {
+
+    console.log(user);
+
+    return {
+      id: user.id,
+      points: user.points
+    }
+  }
+
+  ) : []
+
+  console.log(data, ' pointsicleCampaignspointsicleCampaignspointsicleCampaignspointsicleCampaigns');
   
   return (
     <>
-      <CampaignPage props={data}>
-      </CampaignPage>
+      <div className="md:hidden">
+
+      </div>
+      <div className="hidden max-w-screen-xl w-full mt-16 mx-auto h-full flex-1 flex-col space-y-8 p-8 md:flex">
+        <div className="flex items-center justify-between space-y-2">
+          <div>
+            <h2 className="text-4xl font-bold tracking-tight">{data.data.pointsicleCampaigns[0].displayName} Campaign</h2>
+            <p className="text-xl text-muted-foreground">
+              {data.data.pointsicleCampaigns[0].description}
+            </p>
+          </div>
+        </div>
+
+        <section className="w-full mb-10">
+          <h2 className="text-2xl font-bold mb-5">Quests</h2>
+          <p className="text-lg text-muted-foreground">
+            Complete these quests to earn prizes:
+          </p>
+          <div className="w-full rounded-2xl bg-gray-50 border border-gray-200 overflow-hidden">
+
+            {
+              data.data.quests.map((quest: any, index: number) => {
+
+                return (
+                  <div key={quest.id} className="flex justify-between items-center border-b-2 border-gray-200">
+                    <div className="w-full text-left p-5 focus:outline-none">
+                      <h3 className="font-semibold text-lg">Quest {index + 1}</h3>
+                      <p className="text-gray-500">{quest.description}</p>
+                    </div>
+                    <span className="mr-8 text-right w-24 whitespace-nowrap font-semibold">+{quest.points} Points</span>
+                  </div>
+                )
+              })
+            }
+
+          </div>
+        </section>
+        <hr />
+        <h2 className="text-2xl font-bold">Leaderboard</h2>
+        <p className="text-lg text-muted-foreground">
+          Top 100 Participants:
+        </p>
+
+        <DataTable data={tasks} columns={columns} />
+      </div>
     </>
   );
 };
